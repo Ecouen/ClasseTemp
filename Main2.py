c60c3ce46274ddca64283a235465ce506e35ea48
Ecouen <diecouen@protonmail.com>
2019-12-13 15:41:29 +0100

    update Readme & code

--- Main2.py
+++ Main2.py
@@ -1,23 +1,3 @@
-'''
-Le minimum de votre class AutoML  :
--prétraitement
-   reduction de la taille de votre dataset (float64 => float32, object => category etc.)
-   - sur les variables catégoriques :
-        - gestion des nan
-        - label encoder
-    - sur les variables numériques:
-         - gestion des nan
-        - normalisation
-prediction :
-    - détecter automatiquement si c'est un problème de classification ou de régression
-    - en fonction, utiliser les algos appropriés
-    -  ressortir les performances du modèle
-get_ypred('test.csv') :
-     return ypred
-
-save_ypred('test.csv') => sauvegarde le fichier de prediction prêt à être soumis sur kaggle
-'''
-
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
